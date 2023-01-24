673b5a2a1bd77eb69dfe55666bb1eafbf02939e0
Roger Augusto <56980703+rgr14@users.noreply.github.com>
2023-01-24 20:17:32 -0300

    CourseService

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { Course } from './entities/course.entity';
 
 @Injectable()
-export class CoursesService {}
+export class CoursesService {
+    private courses: Course[] = [
+        {
+            id: 1,
+            name: 'Fundamentos do NestJS',
+            description: 'Description for course #1',
+            tags: ['node.js', 'nestjs', 'javascript']
+        }
+    ];
+
+    findAll(): Course[] {
+        return this.courses;
+    }
+
+    findOne(id: string) {
+        const course = this.courses.find(course => course.id === +id);
+        if(!course) {
+            throw new HttpException(`Course ID ${id} not found`, 404);
+        }
+        return course;
+    }
+
+    create(createCourseDto: any) {
+        this.courses.push(createCourseDto);
+    }
+
+    update(id: string, updateCourseDto: any) {
+        const index = this.courses.findIndex(course => course.id === +id);
+        this.courses[index] = updateCourseDto;
+    }
+
+    remove(id: string) {
+        const indexCourse = this.courses.findIndex(course => course.id === +id);
+        if(indexCourse >= 0) {
+            this.courses.splice(indexCourse, 1);
+        }        
+    }
+}
