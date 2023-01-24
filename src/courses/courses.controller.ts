bd575ceef1d19b62ecbce03902c8a4ccc66faabb
Roger Augusto <56980703+rgr14@users.noreply.github.com>
2023-01-24 20:17:24 -0300

    CoursesController

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,14 +1,34 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Body, Post, Patch, Delete } from '@nestjs/common';
+import { CoursesService } from './courses.service';
+import { createCourseDto } from './dto/courses-dto';
+import { updateCourseDto } from './dto/update-course-dto';
 
 @Controller('courses')
 export class CoursesController {
+    constructor(private readonly coursesService: CoursesService) {}
+
     @Get()
-    findAll(): string {
-        return 'This action returns all courses';
+    findAll() {
+        return this.coursesService.findAll();
     }
 
     @Get(':id')
-    findOne(@Param('id') id): string {
-        return `This action returns a #${id} course`;
+    findOne(@Param('id') id: string) {
+        return this.coursesService.findOne(id);
+    }
+
+    @Post()
+    create(@Body() createCourseDto: createCourseDto) {
+        return this.coursesService.create(createCourseDto);
+    }
+
+    @Patch(':id')
+    update(@Param('id') id:string, @Body() updateCourseDto: updateCourseDto){
+        return this.coursesService.update(id, updateCourseDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id:string){
+        return this.coursesService.remove(id);
     }
 }
