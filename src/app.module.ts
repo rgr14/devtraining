bd467f614f223678559ffcb2d540005e62cd7ea5
Roger Augusto <56980703+rgr14@users.noreply.github.com>
2023-01-26 14:19:54 -0300

    fix: Controller Courses

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,11 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [CoursesModule, TypeOrmModule.forRoot(
-    {
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '@admin123',
-      database: 'devtraining',
-      autoLoadEntities: true,
-      synchronize: true
-    }
-  )],
+  imports: [CoursesModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -14,7 +14,7 @@ export class CoursesController {
 
     @Get(':id')
     findOne(@Param('id') id: string) {
-        return this.coursesService.findOne(id);
+        return this.coursesService.findOne(+id);
     }
 
     @Post()
@@ -24,11 +24,11 @@ export class CoursesController {
 
     @Patch(':id')
     update(@Param('id') id:string, @Body() updateCourseDto: updateCourseDto){
-        return this.coursesService.update(id, updateCourseDto);
+        return this.coursesService.update(+id, updateCourseDto);
     }
 
     @Delete(':id')
     remove(@Param('id') id:string){
-        return this.coursesService.remove(id);
+        return this.coursesService.remove(+id);
     }
 }
